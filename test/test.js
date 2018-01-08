3f197f4ee853274b4daf7376f9c10503bb363c43
tamarefrat <tamarsamson94@gmail.com>
2018-01-08 18:12:33 +0200

    sara

--- test/test.js
+++ test/test.js
@@ -20,3 +20,6 @@ describe('Cash Register', function(){
     })
   })
 });  
+it('getChange(210,300) should equal [50,20,20]', function(){
+    assert.deepEqual(C.getChange(210,300), [50,20,20]);
+})
